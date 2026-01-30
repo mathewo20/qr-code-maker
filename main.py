63cec8ef8ce7708b1017dd5e3d9d0b5c3f29bfcd
mathewo20 <mo237704937@gmail.com>
2026-01-30 14:37:36 +0000

    find out how to make the code without ai

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import qrcode
 
 print("what link do you want me to create the qrcode")
 
-data = input('')
-
-
-
+data = input('enter link: ')
 
 
 img = qrcode.make(data)
-name_for_qrcode = input('')
-img.save('name_for_qrcode'.png)
+
+name_for_qrcode = input("what do you want your qr code name to be?: ")
+
+
+
+img.save(name_for_qrcode, '.png')
+
+
+
+
 
